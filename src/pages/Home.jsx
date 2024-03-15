949e5802d4bc377aaa395544745da4e5921ed78d
MHassanSaleem <hasansalim749@gmail.com>
2024-03-15 01:29:57 +0100

    setting up basic structure , working

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Vegetarian from "../components/Vegetarian";
+import Popular from "../components/Popular";
+
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <Popular/>
+        <Vegetarian/>
+    </div>
+  )
+}
+
+export default Home
